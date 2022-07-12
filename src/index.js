9c2407f9756ed134a117580689d282aee90474b9
Viviana Ruales <108638400+ViviRuales@users.noreply.github.com>
2022-07-12 12:02:04 -0500

    Prueba

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const buttonSend = document.getElementById("SendtoCode");
+
+buttonSend.addEventListener("click", function () {
+    const textToCode = document.getElementById("textToCode").value;
+    const positionToMove = document.getElementById("positionToMove").value;
+    console.log(textToCode, positionToMove);
+});
