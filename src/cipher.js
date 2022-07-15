9dbd7ac55b07b5d21feab1e660c203097e86b8d4
Viviana Ruales <108638400+ViviRuales@users.noreply.github.com>
2022-07-14 22:39:51 -0500

    recorrer letra por letra

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,17 @@
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const cipher = {
   // parametros y argumentos en una funcion de js
   // objetos en js
 
-  encode: function(positionToMove,textToCode) {
-    console.log(textToCode, positionToMove);
-    // Comienza a cifrar
+ encode: function(positionToMove,textToCode) {
+
+    for (var elemento of textToCode) { 'HOLA VIVI'
+        console.log(elemento);
+    }
+    // recorrer una a una las letras de textToCode Ciclo o bucle
     // recorrer con un bucle/ciclo todo el positionToMove
   },
-  decode: function() {}
+  decode: function cifrar() {}
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-const buttonSaludar = document.getElementById("saludar");
-
-function saludar(valor1, valor2) {
-    return valor1 + ' ' + valor2;
-}
-
-buttonSaludar.addEventListener('click', () => {
-    console.log(saludar('Viviana', 'Ruales'));
-})
+const buttonSend = document.getElementById("SendtoCode");
 
 buttonSend.addEventListener("click", function () {
     const textToCode = document.getElementById("textToCode").value;
     const positionToMove = document.getElementById("positionToMove").value;
     //console.log(textToCode, positionToMove);
-    cipher.encode(positionToMove,textToCode)
+    cipher.encode(positionToMove,textToCode) //Parametros y argumentos en una función js 
 
 
 });
