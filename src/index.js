162f9bc92e11938451f12516f51234652d24a5e1
Viviana Ruales <108638400+ViviRuales@users.noreply.github.com>
2022-07-26 23:31:32 -0500

    CSS Página 1 y parte página 2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import cipher from './cipher.js';
 //console.log(textToCode, positionToMove);
 //Parametros y argumentos en una función js 
 
-const botonCifrar = document.getElementById("Codificar");
+/* const botonCifrar = document.getElementById("Codificar");
 
 botonCifrar.addEventListener("click", function (){
     const textToCode = document.getElementById("textToCode").value;
     const positionToMove = document.getElementById("positionToMove").value;
     cipher.encode(positionToMove, textToCode)  
+}) */
+
+const botonStart = document.getElementById("start");
+botonStart.addEventListener("click", function(){
+    document.getElementById("ocultar").style.display = 'none';
+    document.getElementById("Page2").style.display = 'block';
 })
+
