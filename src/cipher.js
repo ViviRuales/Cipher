e5cc321ce7bcf8ffa560667310c719f035a1bb6c
Viviana Ruales <108638400+ViviRuales@users.noreply.github.com>
2022-07-28 12:27:00 -0500

    pasar el index al acsii

--- src/cipher.js
+++ src/cipher.js
@@ -1,25 +1,31 @@
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const indexToAcsii = {
+   (alphabet[index].charCodeAt()- 65+offset)%26+65;}
+
 const cipher = {
   // parametros y argumentos en una funcion de js
   // objetos en js
-  cipherFormula:function(Asciicharacter,positionToMove) {
-    return (Asciicharacter-65+positionToMove)%26+65;
-  },
- encode: function(positionToMove,textToCode) {  
-    for (var elemento of textToCode) {
+  
+ encode: function(offset,string) { 
+
+    /*for (var elemento of textToCode) {
       
       for (let index = 0; index < alphabet.length; index++) {
        
         if(elemento == alphabet[index]){
-          console.log(String.fromCharCode(this.cipherFormula(textToCode.charCodeAt(index),positionToMove)))
+          
         }
         
       }
         
-    } 
+    } */
     // recorrer una a una las letras de textToCode Ciclo o bucle
-    // recorrer con un bucle/ciclo todo el positionToMove
-  },
+  
+  // recorrer con un bucle/ciclo todo el positionToMove
+  console.log(alphabet[index].charCodeAt())
+  return 
+   
+ },
   decode: function() {}
 };
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import cipher from './cipher.js';
 //console.log(textToCode, positionToMove);
 //Parametros y argumentos en una función js 
 
-/* const botonCifrar = document.getElementById("Codificar");
+const botonCifrar = document.getElementById("Codificar");
 
 botonCifrar.addEventListener("click", function (){
     const textToCode = document.getElementById("textToCode").value;
     const positionToMove = document.getElementById("positionToMove").value;
-    cipher.encode(positionToMove, textToCode)  
-}) */
+    cipher.encode(positionToMove, textToCode) 
+    var resultEncode = cipher.encode(positionToMove,textToCode)
+    console.log(resultEncode)
+}) 
 
 const botonStart = document.getElementById("start");
 botonStart.addEventListener("click", function(){
